6e3bd6973211be3d55b160a631bfcb4aeb44cd89
Lurdes Fernandes <lurdes.a.fernandes@gmail.com>
2025-02-10 22:28:34 +0000

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print(soma(3,4));
+
+    }
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+}
